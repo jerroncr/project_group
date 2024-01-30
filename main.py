424f11b590c0a91c58a022ffa86de45ff91de84a
jerroncr <s10262110@connect.np.edu.sg>
2024-01-30 23:06:38 +0800

    MEMBER DETAILS

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import overheads, cash_on_hand, profit_loss
 
-#OPEN TXT FILE
-#WRITING TEAM MEMBERS NAME INTO FILE
-with open("team_members.txt","w") as file:
-    file.write("Swee Guan\n")
-    file.write("Jerron Chia\n")
-    file.write("Raeanne\n")
-    file.write("Elyse\n")
-    file.write("Gwendolyn\n")
-
 def main():
     '''
     - IMPORTING ALL FUNCTIONS FROM ALL FILES
